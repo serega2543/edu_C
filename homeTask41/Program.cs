c61cf6fd0d20d6019a0a91da1e8307de079801c0
SergeyIvanov <serega2543@yandex.ru>
2022-10-24 16:19:47 +0600

    homeTask#6

---
+++ homeTask41/Program.cs
@@ -0,0 +1,46 @@
+﻿/* Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3 */
+
+Console.Clear();
+Console.WriteLine("Введите множестово числе через пробел: ");
+string row = Console.ReadLine();
+
+int[] newArray = StrToArray(row);
+int positivCount = CountPositiveNumber(newArray);
+PrintResilt(newArray, positivCount);
+
+
+int[] StrToArray(string row)
+{
+    int[] arr = Array.ConvertAll(row.Split(), int.Parse);
+    return arr;
+}
+
+int CountPositiveNumber(int[] arr)
+{
+    int count = 0;
+    
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+ 
+void PrintResilt(int[] arr, int result)
+{
+
+    for (int i = 0; i <= arr.Length; i++)
+    {
+        if (i == 0) { Console.Write($"{arr[i]}, "); };
+        if (i > 0 && i < arr.Length - 1) { Console.Write($"{arr[i]}, "); };
+        if (i == arr.Length - 1) { Console.Write($"{arr[i]}"); };
+    }
+    Console.Write(" -> ");
+    Console.Write(result);
+
+}
---
+++ homeTask43/Program.cs
@@ -0,0 +1,41 @@
+﻿/* Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+значения b1, k1, b2 и k2 задаются пользователем.
+ b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)*/
+
+Console.Clear();
+int b1 = InData("b1");
+int k1 = InData("k1");
+int b2 = InData("b2");
+int k2 = InData("k2");
+
+double[] interLineas = IntersectionLines(b1, b2, k1, k2);
+double x = RoundingNum(interLineas[0], 3);
+double y = RoundingNum(interLineas[1], 3);
+
+Console.WriteLine($"b1 = {b1}, k1 = {k1}, b2 = {b2}, k2 = {k2} -> ({x}, {y})");
+
+
+
+double[] IntersectionLines(double b1, double b2, double k1, double k2)
+{
+    //y = k1 * x + b1
+    //y = k2 * x + b2
+    //k1 * x + b1 = k2 * x + b2
+    double x = -(b1 - b2) / (k1 - k2);
+    double y = k1 * x + b1;
+
+    double[] arr = new double[2] { x, y };
+    return arr;
+}
+
+double RoundingNum(double n, int bit)
+{
+    return Math.Round(n, bit);
+}
+
+int InData(string point)
+{
+    Console.WriteLine($"Введите {point}:");
+    int res = int.Parse(Console.ReadLine());
+    return res;
+}
