b6cbe156d4c24db0f85faf97507ea07f1fcc255f
IvanovSergey <serega2543@yandex.ruy>
2022-11-09 01:01:19 +0600

    практическое занятие 8

---
+++ task57/Program.cs
@@ -0,0 +1,59 @@
+﻿// Задача 57: Составить частотный словарь элементов двумерного массива.
+//Частотный словарь содержит информацию о том, сколько раз встречается элемент входных данных.
+
+
+Console.Clear();
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine());
+
+int minValue = 0;
+int maxValue = 5;
+
+int[,] array = GetArray(rows, columns, minValue, maxValue);
+PrinTtwoDimensionalArray(array);
+CountElements(array, minValue, maxValue);
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+
+void PrinTtwoDimensionalArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void CountElements(int[,] arr, int minValue, int maxValue)
+{
+    for (int num = minValue; num <= maxValue; num++)
+    {
+        int count = 0;
+        for (int i = 0; i < arr.GetLength(0); i++)
+        {
+            for (int j = 0; j < arr.GetLength(1); j++)
+            {
+                if (arr[i, j] == num) {count++;}
+            }
+        }
+        if(count>0)Console.WriteLine($"Число {num} встречается {count} раз");
+    }
+
+}
